57749008bf567ea62146c0e0e25d48bc775bbd6c
Charles Bergeron <chuck.bergeron@gmail.com>
2018-05-07 16:44:55 -0700

    * remove a console log

--- app/javascripts/contracts/nftoken-factory.js
+++ app/javascripts/contracts/nftoken-factory.js
@@ -7,9 +7,5 @@ export default async function () {
   nfToken.setProvider(web3.currentProvider)
   nfToken.web3.eth.defaultAccount = web3.eth.accounts[0]
 
-  console.log('in nfToken Factory')
-
-  // TODO: Will need to get this from networks json array
-  // return nfToken.at()
   return nfToken.deployed()
 }
