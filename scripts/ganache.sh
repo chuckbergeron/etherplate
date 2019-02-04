619e23b724e357f53c316f3691053c3fff549f8c
Charles Bergeron <chuck.bergeron@gmail.com>
2019-02-03 23:08:16 -0800

    * add better settings to ganache and bump the truffle version

--- scripts/ganache.sh
+++ scripts/ganache.sh
@@ -2,9 +2,10 @@
 mkdir -p .ganache
 ganache-cli \
   --db .ganache \
-  -l 4700038 \
+  -l 8000038 \
   -i 1234 \
-  -e 100000000000 \
+  -e 10000 \
   -a 10 \
+  -g 100000000000 \
   -u 0 \
   -m "$HDWALLET_MNEMONIC"
